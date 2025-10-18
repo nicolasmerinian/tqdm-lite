04d5d24b6e057b94964ffaf6d862b3414775f7a2
Nicolas Merinian <nicolas.merinian@gmail.com>
2025-10-18 22:50:37 +0200

    working progress bar

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-def tqdm_lite(iterable):    
+import sys
+import time
+
+def tqdm_lite(iterable):
+    total = len(iterable)
+    
     for i, item in enumerate(iterable):
+        percent = i / total * 100
+        progress = "#" * int(percent)
+        
+        sys.stdout.write(f"\r{progress}")
+        sys.stdout.flush()
+
         yield item
-
-
+    
+    
 def main():
     array = [x ** 2 for x in range(10)]
     
-    for i in tqdm_lite(array):
-        print(i)
+    for _ in tqdm_lite(array):
+        time.sleep(0.15)
+
 
 
 if __name__ == "__main__":
