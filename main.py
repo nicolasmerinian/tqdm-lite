5b0db9e75a9245f1367b4dd9fd72bfe9f630ee81
Nicolas Merinian <nicolas.merinian@gmail.com>
2025-10-19 13:49:18 +0200

    added remaining time display

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ class TqdmLite:
             self.total = len(self.iterable)
         except TypeError:
             self.total = None
+        self.timer = None
     
-    def _print_bar(self, progress_ratio):
+    def _print_bar(self, progress_ratio, index):
         filled = int(progress_ratio * self.bar_length)
         bar = "#" * filled + "-" * (self.bar_length - filled)
-        sys.stdout.write(f"\r{bar} | {progress_ratio * 100:.1f}%")
+        elapsed = time.time() - self.timer
+        eta = elapsed / index * (self.total - index)
+        sys.stdout.write(f"\r{bar} | {progress_ratio * 100:.1f}% | Remaining: {eta:.1f}s")
         sys.stdout.flush()
     
     def __iter__(self):
+        self.timer = time.time()
+        
         if self.total is not None:
             for i, item in enumerate(self.iterable, start=1):
                 yield item
-                self._print_bar(i / self.total)
+                self._print_bar(i / self.total, i)
         else:
             for i, item in enumerate(self.iterable, start=1):
                 yield item
