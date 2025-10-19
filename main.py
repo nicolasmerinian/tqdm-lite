ff855dfb9f308bdddac119e8780e74bb5f7db1b4
Nicolas Merinian <nicolas.merinian@gmail.com>
2025-10-19 13:03:45 +0200

    try/except in case of enumerable without len()

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import sys
 import time
 
 def tqdm_lite(iterable):
-    total = len(iterable)
+    try:
+        total = len(iterable)
+    except TypeError:
+        total = None
     bar_length = 25
     
     for i, item in enumerate(iterable):
-        percent = i / total
-        progress = int(percent * bar_length)
-        bar = "#" * progress + '-' * (bar_length - progress)
-        sys.stdout.write(f"\r{bar} | {percent*100:.1f}%")
-        sys.stdout.flush()
+        if total is not None:
+            percent = i / total
+            progress = int(percent * bar_length)
+            bar = "#" * progress + '-' * (bar_length - progress)
+            sys.stdout.write(f"\r{bar} | {percent*100:.1f}%")
+            sys.stdout.flush()
 
         yield item
     
-    bar = "#" * bar_length
-    sys.stdout.write(f"\r{bar} | 100.0%")
-    sys.stdout.flush()
+    if total is not None:
+        bar = "#" * bar_length
+        sys.stdout.write(f"\r{bar} | 100.0%")
+        sys.stdout.flush()
     
 
 def main():
